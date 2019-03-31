b568f42e839f9a4005adfc732fd4f48d9d9d2475
twn39 <twn39@163.com>
2019-03-31 18:00:17 +0800

    update

---
+++ build.php
@@ -0,0 +1,23 @@
+<?php
+$exts = ['php'];            // 需要打包的文件后缀
+$dir = __DIR__;             // 需要打包的目录
+$file = 'nb.phar';       // 包的名称, 注意它不仅仅是一个文件名, 在stub中也会作为入口前缀
+$phar = new Phar(__DIR__ . '/' . $file, FilesystemIterator::CURRENT_AS_FILEINFO | FilesystemIterator::KEY_AS_FILENAME, $file);
+// 开始打包
+$phar->startBuffering();
+// 将后缀名相关的文件打包
+foreach ($exts as $ext) {
+    $phar->buildFromDirectory($dir, '/\.' . $ext . '$/');
+}
+// 把build.php本身摘除
+$phar->delete('build.php');
+// 设置入口
+$defaultStub = $phar->createDefaultStub('bin/nb.php');
+$defaultStub = <<<STUB
+#!/usr/bin/env php
+$defaultStub
+STUB;
+$phar->setStub($defaultStub);
+$phar->stopBuffering();
+// 打包完成
+echo "Finished {$file}\n";
